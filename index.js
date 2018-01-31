eb7856bb24ef743b330cc3410d2fbe5cc4770ee0
Pitta <mailez4all@gmail.com>
2018-01-31 03:19:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logWhisper(hello) {
 function sayHiToGrandma(hello) {
   if ('hello'.toLowerCase() === 'hello') {
     return "I can't hear you!"
-  } else if (uppercase.toUpperCase() === uppercase) {
+  } else if ('HELLO'.toUpperCase() === 'HELLO') {
     return "YES INDEED!"
   } else {
     return "I love you, Grandma."

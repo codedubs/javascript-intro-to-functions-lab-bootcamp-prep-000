087fb8bfc420f3ca290377bcb8d9f1b5e963eb9f
Pitta <mailez4all@gmail.com>
2018-01-31 02:02:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(hello) {
+  return 'hello'.toUpperCase()
+}
+
+function whisper(hello) {
+  return 'hello'.toLowerCase()
+}
+
+function logShout(hello) {
+
+  console.log('hello'.toUpperCase())
+}
+
+function logWhisper(hello) {
+    console.log('hello'.toLowerCase())
+}
+
+
+var string = string.toLowerCase()
+
+function sayHiToGrandma(string) {
+
+    return "I can't hear you!"
+
+}
+
+function sayHiToGrandma(hellos) {
+  if (hellos === 'hellos'.toUpperCase()) {
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(hella) {
+  if (hella === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
